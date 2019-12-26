0aebf7405f397630c463226181d3602559254781
Hax <645853712@qq.com>
2019-12-26 22:38:14 +0800

    今晚基本没什么收获

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { GlobalStyle } from  './style';
-import {IconStyle} from './assets/iconfont/iconfont'
+import {IconStyle} from './assets/iconfont/iconfont';
+import routes from './routes/index.js';
+import { renderRoutes } from 'react-router-config';
+import { HashRouter } from 'react-router-dom';
 
 export function App(){
     return(
-        <>
+        <HashRouter>
             <GlobalStyle />
             <IconStyle />
-            <i className="iconfont">&#xe62b;</i>
-        </>
+            {renderRoutes(routes)}
+        </HashRouter>
     );
 }
