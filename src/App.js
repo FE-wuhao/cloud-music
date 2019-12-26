75a6a1ad8ad30787c048fff6935517bdfc8a95b3
Hax <645853712@qq.com>
2019-12-26 17:54:08 +0800

    整明白了。。。就是后面章节要用的给提前写上了，怪不得呢，误导。。

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from  './style';
+import {IconStyle} from './assets/iconfont/iconfont'
 
 export function App(){
     return(
-        <GlobalStyle/>
+        <>
+            <GlobalStyle />
+            <IconStyle />
+            <i className="iconfont">&#xe62b;</i>
+        </>
     );
 }
