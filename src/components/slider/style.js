767076e80dfc6335d0e00211de327c9fc33df450
Hax <645853712@qq.com>
2020-01-09 17:57:35 +0800

    好几天没做这个项目了，复习一下前面的内容，并对作者设置的CSS提出质疑，质疑成功，耶!

--- src/components/slider/style.js
+++ src/components/slider/style.js
@@ -7,7 +7,8 @@ export const SliderContainer = styled.div`
   width: 100%;
   /* 大致可以判断这里的height是无意义的，
      因为百分比高度取决于父元素的高度，它的父元素使body和html，
-     如果不设置body和html的高度，那么百分比高度设置就是无效的 */
+     如果不设置body和html的高度，那么百分比高度设置就是无效的，
+     所以它的高度只取决于它内部的轮播图的高度 */
   /* height: 100%;  */
   /* margin: auto; */
   background: white;
@@ -22,12 +23,15 @@ export const SliderContainer = styled.div`
     position: relative;
     width: 98%;
     height: 160px;
-    overflow: hidden;
+    overflow: hidden;/* 设置为一旦元素溢出就隐藏 */
     margin: auto;
     border-radius: 6px;
     .slider-nav {
-      position: absolute;
-      display: block;
+      /* 不能理解为什么要设置为绝对定位，
+        不还是相对于父元素进行高度设置吗？
+        直接设置height：100%不就达成目的了吗。。。 */
+      /* position: absolute;
+      display: block; */
       width: 100%;
       height: 100%;
     }
